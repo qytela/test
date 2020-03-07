199a72fa2ace2847c8d9750bc555ec576d7d2f7d
Qytela <julfanshahidayah@gmail.com>
2020-03-07 12:22:16 +0000

    s

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel with Jenkins and Docker (CI / CD)
+                    Laravel with Jenkins and Docker
                 </div>
 
                 <div class="links">
