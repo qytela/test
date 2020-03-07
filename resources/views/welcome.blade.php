312893cf30faa3c45195dc57a7282016748c5dd4
Qytela <julfanshahidayah@gmail.com>
2020-03-07 06:02:31 +0000

    M

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel with Jenkins and Docker
                 </div>
 
                 <div class="links">
