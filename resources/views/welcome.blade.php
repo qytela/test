3d422b0457722b5c961406ddd1e6c212eeca130c
rizkyawliafajrin <rizkyawliafajrin@gmail.com>
2020-03-11 14:29:13 +0700

    test update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel with Jenkins and Docker by Fansa
+                    Laravel with Jenkins and Docker by Fansa & Evan & Daniel & Dewi
                 </div>
 
                 <div class="links">
